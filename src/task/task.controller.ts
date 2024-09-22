9196eb0bd8873dbf0651c0c9ae0d7ef82f44f4fd
Jamilson Pestana Jr <jamilsonjr@live.com>
2024-09-22 18:39:59 -0300

    feature: create tasks  create, post and delete

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,13 +1,27 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { TaskDto } from './task.dto';
+import { TaskService } from './task.service';
 
 @Controller('task')
 export class TaskController {
 
+    constructor(private readonly taskService: TaskService) {}
+
     @Post()
     create(@Body() task: TaskDto){
-        console.log(task);
-        console.log('hello world!!!!!!!');
+       return this.taskService.create(task);
     }
 
+    @Get('/:id')
+    // o Param é um decorator que tem acesso aos parametros da rota
+    findById(@Param('id') id:string) : TaskDto{
+        return this.taskService.findById(id);
+    }
+
+    @Put()
+    update(@Body() task: TaskDto){
+        return this.taskService.update(task);
+    }
+    
 }
+ 
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,4 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { TaskDto } from './task.dto';
 
 @Injectable()
-export class TaskService {}
+export class TaskService {
+
+    private tasks: TaskDto[] = [];
+
+    create(task: TaskDto){
+        this.tasks.push(task);
+        return task;
+    }
+
+    findById(id: string){
+        const foundTask = this.tasks.find(task => task.id === id);
+        
+        if(foundTask){
+            return foundTask;
+        }
+        
+        throw new HttpException(`Task with id = ${id} not found`, HttpStatus.NOT_FOUND);
+    }
+
+    update(task: TaskDto){
+        let taskIndex = this.tasks.findIndex(task => task.id === task.id);
+
+        if(taskIndex === -1){
+            throw new HttpException(`Task with id = ${task.id} not found`, HttpStatus.BAD_REQUEST);
+        }
+
+        this.tasks[taskIndex] = task;
+
+        return this.tasks[taskIndex];
+
+    }
+
+}
